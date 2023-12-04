fad0744ae53cd144fc2556c5d3829bbc80e833c3
TyuiuFaizullinDR@outlook.com <toxen2005@mail.ru>
2023-12-04 21:11:39 +0500

    Task5

--- Tyuiu.FaizullinDR.Sprint6.Task5.V11.Test/DataServiceTest.cs
+++ Tyuiu.FaizullinDR.Sprint6.Task5.V11.Test/DataServiceTest.cs
@@ -10,6 +10,11 @@ namespace Tyuiu.FaizullinDR.Sprint6.Task5.V11.Test
         [TestMethod]
         public void TestMethod1() 
         {
+            string path = @"C:\DataSprint6\InPutFileTask5V11.txt";
+            DataService ds = new DataService();
+            double[] res = ds.LoadFromDataFile(path);
+            double[] wait = { 10 };
+            CollectionAssert.AreEqual(wait, res);
         }
     }
 }
